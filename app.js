f86dc26e122e772f96fa4bb009a38f3287c8b9e6
DenisOlegovich <93161722+DenisOlegovich@users.noreply.github.com>
2021-11-17 23:24:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const list = {
-    "create a task": "In Progress",
-    "make a bed": "Done",
-    "write a post": "To Do",
-   }
-function changeStatus(task, changetask){
-    list[task] = changetask
-}
+const list = [ 
+    { 
+        id: 1,
+        name: 'create a post', 
+        status: 'TODO', 
+        priority: 'low'  
+    }, 
+    { 
+        id: 2,
+        name: 'test', 
+        status: 'Done', 
+        priority: 'high'  
+    } 
+    ] 
 
-function addTask(task){
-    list[task] = "To Do"
+  
+function changeStatus(task, changeTask){
+    list[list.findIndex(item => item.name == task)].status = changeTask 
+};
+
+
+function addTask(name, id = list.length + 1, status = "TODO", priority = "low"){
+  let taskObj = []
+  taskObj.id = id
+  taskObj.name = name
+  taskObj.status = status
+  taskObj.priority = priority
+    list.push(taskObj)
 }
 
 function deleteTask(task){
-    delete list[task]
+    list.splice(list.findIndex(item => item.name == task), 1)
 }
 
 function showList(){
@@ -21,26 +38,26 @@ function showList(){
     let inProgress = []
     let done = []
     
-    for(let key in list){
-      if (list[key] === "To Do"){
-        if(toDo.lenght === 0){
-        toDo.push(key)
+    for(let i = 0; i < list.length; i ++){
+      if (list[i].status === "TODO"){
+        if(toDo.length === 0){
+        toDo.push(list[i].name)
         }else{
-        toDo.push("\n" + key)
+        toDo.push("\n" + list[i].name)
         }}
       
-      else if (list[key] === "In Progress"){
+      else if (list[i].status === "In Progress"){
         if(inProgress.length === 0){
-        inProgress.push(key)
+        inProgress.push(list[i].name)
         }else{
-        inProgress.push("\n" + key)
+        inProgress.push("\n" + list[i].name)
         }}
       
-      else if (list[key] === "Done"){
+      else if (list[i].status === "Done"){
       	if(done.length === 0){
-      	done.push(key)
+      	done.push(list[i].name)
       	}else{
-        done.push("\n" + key)
+        done.push("\n" + list[i].name)
       }}
     }
  console.log(
@@ -52,7 +69,11 @@ Done:
 ${done.length > 0 ? done:"-"}`) 
  }
  
-changeStatus("write a post", "Done")
+function showBy(key){
+  
+}
+
+changeStatus("create a post", "Done")
 addTask('have a walk')
 deleteTask('have a walk')
 showList()
